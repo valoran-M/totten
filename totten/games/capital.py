bf7eb1f54c70e1d4c92c3f92912bb1118cb8174e
Valeran-M <v.maytie85@orange.fr>
2021-04-01 23:04:44 +0200

    correct Capital

--- totten/games/capital.py
+++ totten/games/capital.py
@@ -8,29 +8,31 @@ class Capital:
 
     def __init__(self, message):
         self.channel = message.channel
+        self.author = message.author
         f = open("images/concap.csv","r")
         table = list(csv.reader(f, delimiter=','))
         f.close()
         n = randint(1,len(table))
         pays = table[n]
         self.nom = pays[0]
-        self.capitale = pays[1]
+        self.capitale = pays[1].lower()
 
     async def launch(self, client):
         def is_correct(m):
-            return m.channel == self.channel
+            return m.channel == self.channel and m.author == self.author
 
         await self.channel.send(
             f"What is the capital of : {self.nom} ? "
         )
         print(self.capitale)
-        guess = await client.wait_for(event="message", check=is_correct)
+        message = await client.wait_for(event="message", check=is_correct)
+        guess = message.content.lower()
         if guess == self.capitale:
             await self.channel.send(
-                f"{guess.author.mention}\n🎉Well done, the capital was {self.capitale}🎉"
+                f"{message.author.mention}\n🎉Well done, the capital was {self.capitale}🎉"
             )
         else:
             await self.channel.send(
-                f"{guess.author.mention}\nSorry, the capital was {self.capitale}"
+                f"{message.author.mention}\nSorry, the capital was {self.capitale}"
             )
 
