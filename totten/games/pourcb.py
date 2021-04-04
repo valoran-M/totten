8922c4c159ac9ad222c0e3137f89ba81b90be961
BoubouSW <gilliclement@gmail.com>
2021-04-04 14:40:31 +0200

    add number choice pourcb

--- totten/games/pourcb.py
+++ totten/games/pourcb.py
@@ -12,15 +12,15 @@ class Pourcb:
         self.content = message.content
         self.channel = message.channel
         self.mention1 = message.mentions[0]
-        #commande = message.content.split(" ")
-        #self.number = int(commande[2]) if len(commande) >= 3 and commande[2].isdigit() else 10
+        commande = message.content.split(" ")
+        self.number = int(commande[2]) if len(commande) >= 3 and commande[2].isdigit() else 10
         #self.mention2 = message.mentions[1]
 
 
     async def launch(self, client):
 
-        await self.mention1.send(f"Donne un nombre entre 1 et 10")  
-        await self.author.send(f"Donne un nombre entre 1 et 10") #mention2->author
+        await self.mention1.send(f"Donne un nombre entre 1 et {self.number}")  
+        await self.author.send(f"Donne un nombre entre 1 et {self.number}") #mention2->author
 			
         def is_correct1(m):
             return m.author == self.mention1 and m.content.isdigit() and isinstance(m.channel, DMChannel)
@@ -33,6 +33,6 @@ class Pourcb:
         rep2 = int(guess2.content)
         print(rep1,rep2)
         if rep1 == rep2:
-            await self.channel.send(f'Gagné, vous avez tous les deux choisi {rep1}')
+            await self.channel.send(f'{self.mention1.mention} {self.author.mention}\nGagné, vous avez tous les deux choisi {rep1}')
         else :
             await self.channel.send(f'Perdu, {self.mention1.mention} a choisi {rep1} et {self.author.mention} a choisi {rep2}')
